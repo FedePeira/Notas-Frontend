b14ae6bd1cfed02fce79c67a9b3d37c1c6427a05
Federico Peirano <fedepr2345@gmail.com>
2024-03-12 12:00:09 -0300

    d. Pruebas de Extremo a Extremo - Practica

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -12,11 +12,11 @@ describe('Note ', function() {
 
   it('front page can be opened', function() {
     cy.contains('Notes')
-    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2024')
   })
 
   it('login form can be opened', function() {
-    cy.contains('log in').click()
+    cy.contains('Login').click()
     cy.get('#username').type('mluukkai')
     cy.get('#password').type('salainen')
     cy.get('#login-button').click()
