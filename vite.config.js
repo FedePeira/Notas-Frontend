fbeba38ddada99898149d61a9fefc41f11b82404
Federico Peirano <fedepr2345@gmail.com>
2024-05-20 14:54:25 -0300

    Render Solution - v1.1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://notas-429a.onrender.com',
         changeOrigin: true,
       },
     }
